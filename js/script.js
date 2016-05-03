5de8207c1cd73bf744c308655b1c2d1fa1b6a75f
Joseph Halderman <josephhalderman@Josephs-MacBook-Pro-2.local>
2016-05-02 21:04:48 -0700

    started final project

---
+++ js/script.js
@@ -0,0 +1,34 @@
+//figure out layout of page - shapes, keys = whatever is going to be clickable to play music
+//give shapes id's so they can be targeted
+
+var html5_audiotypes={
+	"mp3": "audio/mpeg",
+	"mp4": "audio/mp4",
+	"ogg": "audio/ogg",
+	"wav": "audio/wav"
+}
+
+function createsoundbite(sound){
+	var html5audio=document.createElement('audio')
+	if (html5audio.canPlayType){ //check support for HTML5 audio
+		for (var i=0; i<arguments.length; i++){
+			var sourceel=document.createElement('source')
+			sourceel.setAttribute('src', arguments[i])
+			if (arguments[i].match(/\.(\w+)$/i))
+				sourceel.setAttribute('type', html5_audiotypes[RegExp.$1])
+			html5audio.appendChild(sourceel)
+		}
+		html5audio.load()
+		html5audio.playclip=function(){
+			html5audio.pause()
+			html5audio.currentTime=0
+			html5audio.play()
+		}
+		return html5audio
+	}
+	else{
+		return {playclip:function(){throw new Error("Your browser doesn't support HTML5 audio unfortunately")}}
+	}
+}
+
+var clicksound=createsoundbite("")
--- script.js
+++
@@ -1,6 +0,0 @@
-console.log("Hello from the JavaScript file");
-
-
-var randomNumber = Math.floor( (Math.random()) * 100);
-
-document.getElementById("new").innerHTML = randomNumber;
