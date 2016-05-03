bb34e44dff364dae34f85588868bda43f9b6173a
Joseph Halderman <josephhalderman@Josephs-MacBook-Pro-2.local>
2016-05-02 18:00:49 -0700

    made final project repository

---
+++ README.md
@@ -0,0 +1,1 @@
+# geekwise.javascript-template
---
+++ script.js
@@ -0,0 +1,6 @@
+console.log("Hello from the JavaScript file");
+
+
+var randomNumber = Math.floor( (Math.random()) * 100);
+
+document.getElementById("new").innerHTML = randomNumber;
